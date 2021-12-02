d580bdb446b73f78a9c3020f7ac2485b66e01bb8
Elliot J. Voris <elliot@voris.me>
2021-12-02 17:11:25 -0600

    Starting with tests this time.
    Getting some of the simpler functional tests out of the way to help
    construct the validation checks in the api
    
    Signed-off-by: Elliot J. Voris <elliot@voris.me>

--- routes/api.js
+++ routes/api.js
@@ -3,11 +3,17 @@
 const Translator = require('../components/translator.js');
 
 module.exports = function (app) {
-  
+
   const translator = new Translator();
 
   app.route('/api/translate')
     .post((req, res) => {
-      
+      if (req.body.text === '' ) {
+        return res.json({ error: 'No text to translate' })
+      } else if (!req.body.text || !/^[american|british]\-to\-[british|american]$/.test(req.body.locale)) {
+        return res.json({ error: 'Required field(s) missing' })
+      } else {
+        return res.json({ success: 'something happened' })
+      }
     });
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -5,8 +5,117 @@ const server = process.env.LIVE_URL || require('../app.js');
 
 chai.use(chaiHttp);
 
-let Translator = require('../components/translator.js');
+// let Translator = require('../components/translator.js');
 
-suite('Functional Tests', () => {
+suite('Functional Tests', function() {
+  this.timeout(5000)
+
+  suite('Testing the /api/translate endpoint with valid input', () => {
+
+    test('Translation with text and locale fields: POST request to /api/translate', (done) => {
+      let inputObject = {
+        text: 'Here is some text to translate',
+        locale: 'british-to-american'
+      }
+      chai.request(server)
+        .post('/api/translate')
+        .type('json')
+        .send(inputObject)
+        .end( (err, res) => {
+          assert.equal(true, false)
+          done()
+        })
+    })
+
+  })
+
+  suite('Testing the /api/translate endpoint with invalid input', () => {
+
+    test('Translation with text and invalid locale field: POST request to /api/translate', (done) => {
+      let inputObject = {
+        text: 'Here is some text to translate',
+        locale: 'canadian-to-australian'
+      }
+      chai.request(server)
+        .post('/api/translate')
+        .type('json')
+        .send(inputObject)
+        .end( (err, res) => {
+          assert.equal(res.status, 200, 'return status should be 200')
+          assert.isObject(res.body, 'returned object should be of type object')
+          assert.property(res.body, 'error', 'returned object should contain an error property')
+          assert.isString(res.body.error, 'returned error property should be of type string')
+          assert.equal(res.body.error, 'Required field(s) missing', 'returned error should contain a helpful message')
+          done()
+        })
+    })
+    test('Translation with missing text field: POST request to /api/translate', (done) => {
+      let inputObject = {
+        locale: 'british-to-american'
+      }
+      chai.request(server)
+        .post('/api/translate')
+        .type('json')
+        .send(inputObject)
+        .end( (err, res) => {
+          assert.equal(res.status, 200, 'return status should be 200')
+          assert.isObject(res.body, 'returned object should be of type object')
+          assert.property(res.body, 'error', 'returned object should contain an error property')
+          assert.isString(res.body.error, 'returned error property should be of type string')
+          assert.equal(res.body.error, 'Required field(s) missing', 'returned error should contain a helpful message')
+          done()
+        })
+    })
+    test('Translation with missing locale field: POST request to /api/translate', (done) => {
+      let inputObject = {
+        text: 'Here is some text to translate',
+      }
+      chai.request(server)
+        .post('/api/translate')
+        .type('json')
+        .send(inputObject)
+        .end( (err, res) => {
+          assert.equal(res.status, 200, 'return status should be 200')
+          assert.isObject(res.body, 'returned object should be of type object')
+          assert.property(res.body, 'error', 'returned object should contain an error property')
+          assert.isString(res.body.error, 'returned error property should be of type string')
+          assert.equal(res.body.error, 'Required field(s) missing', 'returned error should contain a helpful message')
+          done()
+        })
+    })
+    test('Translation with empty text: POST request to /api/translate', (done) => {
+      let inputObject = {
+        text: '',
+        locale: 'british-to-american'
+      }
+      chai.request(server)
+        .post('/api/translate')
+        .type('json')
+        .send(inputObject)
+        .end( (err, res) => {
+          assert.equal(res.status, 200, 'return status should be 200')
+          assert.isObject(res.body, 'returned object should be of type object')
+          assert.property(res.body, 'error', 'returned object should contain an error property')
+          assert.isString(res.body.error, 'returned error property should be of type string')
+          assert.equal(res.body.error, 'No text to translate', 'returned error should contain a helpful message')
+          done()
+        })
+    })
+    test('Translation with text that needs no translation: POST request to /api/translate', (done) => {
+      let inputObject = {
+        text: 'Here is some text to translate',
+        locale: 'british-to-american'
+      }
+      chai.request(server)
+        .post('/api/translate')
+        .type('json')
+        .send(inputObject)
+        .end( (err, res) => {
+          assert.equal(true, false)
+          done()
+        })
+    })
+
+  })
 
 });
