fa0bd7403807622a77cdff731c8a1aba354db199
Elliot J. Voris <elliot@voris.me>
2021-12-06 21:42:09 -0600

    Simplifying some of this before I begin the unit tests
    Signed-off-by: Elliot J. Voris <elliot@voris.me>

--- components/translator.js
+++ components/translator.js
@@ -6,11 +6,7 @@ const britishOnly = require('./british-only.js')
 class Translator {
 
   constructor() {
-    this.getTextArray.bind(this)
-  }
-
-  getTextArray(textString) {
-    return textString.split(' ')
+    this.getStartingLocale.bind(this)
   }
 
   getStartingLocale(localeString) {
@@ -18,112 +14,45 @@ class Translator {
     return localeString.match(re)[1]
   }
 
-  needsTranslation(textString) {
-    let translationBool = false
-    for (let w of this.getTextArray(textString)) {
-      if (americanToBritishSpelling[w] || americanToBritishTitles[w]) {
-        translationBool = true
+  translate(textString, localeString) {
+    let returnString = textString
+    let startingLocale = this.getStartingLocale(localeString)
+
+    for (let [k, v] of Object.entries(britishOnly)) {
+      let og = startingLocale === 'american' ? v : k
+      let tr = startingLocale === 'american' ? k : v
+      if (textString.includes(og)) {
+        let re = new RegExp(og, 'g')
+        returnString = returnString.replace(re, `<span class="highlight">${tr}</span>`)
+        // break
+      }
+    }
+    for (let [k, v] of Object.entries(americanToBritishTitles)) {
+      let og = startingLocale === 'american' ? k : v
+      let tr = startingLocale === 'american' ? v : k
+      if (textString.includes(og)) {
+        let re = new RegExp(og, 'g')
+        returnString = returnString.replace(re, `<span class="highlight">${tr}</span>`)
+      }
+    }
+    for (let [k, v] of Object.entries(americanToBritishSpelling)) {
+      let og = startingLocale === 'american' ? k : v
+      let tr = startingLocale === 'american' ? v : k
+      if (textString.includes(og)) {
+        let re = new RegExp(og, 'g')
+        returnString = returnString.replace(re, `<span class="highlight">${tr}</span>`)
       }
     }
     for (let [k, v] of Object.entries(americanOnly)) {
-      if (textString.includes(k)) translationBool = true
+      let og = startingLocale === 'american' ? k : v
+      let tr = startingLocale === 'american' ? v : k
+      if (textString.includes(og)) {
+        let re = new RegExp(og, 'g')
+        returnString = returnString.replace(re, `<span class="highlight">${tr}</span>`)
+      }
     }
-    for (let [k, v] of Object.entries(britishOnly)) {
-      if (textString.includes(v)) translationBool = true
-    }
-    return translationBool
-  }
 
-  translateSpelling(textWord, startingLocale) {
-    let returnWord = textWord
-    switch (startingLocale) {
-      case 'american':
-        if (americanToBritishTitles[textWord]) {
-          returnWord = `<span class="highlight">${americanToBritishTitles[textWord]}</span>`
-        } else if (americanToBritishSpelling[textWord]) {
-          returnWord = `<span class="highlight">${americanToBritishSpelling[textWord]}</span>`
-        }
-        break
-      case 'british':
-        for (let [k, v] of Object.entries(americanToBritishTitles)) {
-          if (v === textWord) returnWord = `<span class="highlight">${v}</span>`
-        }
-        for (let [k, v] of Object.entries(americanToBritishSpelling)) {
-          if (v === textWord) returnWord = `<span class="highlight">${v}</span>`
-        }
-        break
-        // break
-      default:
-        break
-    }
-    return returnWord
-  }
-
-  translatePhrase(textString, startingLocale) {
-    let returnString = textString
-    switch(startingLocale) {
-      case 'american':
-        break
-      case 'british':
-        for (let [k, v] of Object.entries(britishOnly)) {
-          if (textString.includes(k)) {
-            console.log('yes here')
-            let re = new RegExp(k, 'g')
-            returnString = returnString.replace(re, `<span class="highlight">${v}</span>`)
-            break
-          }
-        }
-        break
-      default:
-        break
-    }
-    return returnString
-  }
-
-  translateAll(textString, startingLocale) {
-    let returnString = textString
-    // switch(startingLocale) {
-    //   case 'american':
-    //     break
-    //   case 'british':
-        for (let [k, v] of Object.entries(britishOnly)) {
-          let og = startingLocale === 'american' ? v : k
-          let tr = startingLocale === 'american' ? k : v
-          if (textString.includes(og)) {
-            let re = new RegExp(og, 'g')
-            returnString = returnString.replace(re, `<span class="highlight">${tr}</span>`)
-            // break
-          }
-        }
-        for (let [k, v] of Object.entries(americanToBritishTitles)) {
-          let og = startingLocale === 'american' ? k : v
-          let tr = startingLocale === 'american' ? v : k
-          if (textString.includes(og)) {
-            let re = new RegExp(og, 'g')
-            returnString = returnString.replace(re, `<span class="highlight">${tr}</span>`)
-          }
-        }
-        for (let [k, v] of Object.entries(americanToBritishSpelling)) {
-          let og = startingLocale === 'american' ? k : v
-          let tr = startingLocale === 'american' ? v : k
-          if (textString.includes(og)) {
-            let re = new RegExp(og, 'g')
-            returnString = returnString.replace(re, `<span class="highlight">${tr}</span>`)
-          }
-        }
-        for (let [k, v] of Object.entries(americanOnly)) {
-          let og = startingLocale === 'american' ? k : v
-          let tr = startingLocale === 'american' ? v : k
-          if (textString.includes(og)) {
-            let re = new RegExp(og, 'g')
-            returnString = returnString.replace(re, `<span class="highlight">${tr}</span>`)
-          }
-        }
-        // break
-      // default:
-      //   break
-    // }
-    return returnString
+    return returnString === textString ? 'Everything looks good to me!' : returnString
   }
 
 }
--- routes/api.js
+++ routes/api.js
@@ -14,27 +14,11 @@ module.exports = function (app) {
         return res.json({ error: 'Required field(s) missing' })
       } else if (!/^american\-to\-british|british\-to\-american$/.test(req.body.locale)) {
         return res.json({ error: 'Invalid value for locale field' })
-      // TODO: test for text that doesn't need to be translated. Although,
-      // perhaps that belongs in the translator file?
       } else {
         const textString = req.body.text
-        const locale = req.body.locale
-        let translationArray = []
-        let textArray = translator.getTextArray(textString)
-        let startingLocale = translator.getStartingLocale(locale)
-        // console.log(textArray)
-        // if (translator.needsTranslation(textString)) {
-        //   // do the translation
-        //   for (let w of textArray) {
-        //     translationArray = translationArray.concat(translator.translateSpelling(w, startingLocale))
-        //   }
-        //   let translationString = translator.translatePhrase(translationArray.join(' '), startingLocale)
-        //   return res.json({ text: textString, translation: translationString })
-        // } else {
-        //   return res.json({ text: textString, translation: 'Everything looks good to me!'})
-        // }
-        // We've passed the necessary validations. Here's where we get to work!
-        let translationString = translator.translateAll(textString, startingLocale)
+        const localeString = req.body.locale
+        
+        let translationString = translator.translate(textString, localeString)
         return res.json({ text: textString, translation: translationString })
       }
     });
